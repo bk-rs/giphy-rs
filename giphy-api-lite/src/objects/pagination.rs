aeb97fd7ee811053f3c21b82fda23fa44569dacc
vkill <vkill.net@gmail.com>
2022-10-28 14:59:47 +0800

    Fix pagination

--- giphy-api-lite/src/objects/pagination.rs
+++ giphy-api-lite/src/objects/pagination.rs
@@ -5,6 +5,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Pagination {
     pub offset: u32,
-    pub total_count: u32,
+    pub total_count: Option<u32>,
     pub count: u32,
 }
